c73e01ced4ea6ae6ead86e1166d3a2293728f7d9
Ankit Singh Rajput <ankitsingh@Ankits-MacBook-Air.local>
2026-01-04 11:45:57 +0530

    add new message

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Ankit Singh Rajput");
+        for (int i = 0; i <= 5; i++) {
+            System.out.println(i);
+        }
     }
 }
